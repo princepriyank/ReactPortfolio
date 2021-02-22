4eebd52484d9eba8de918ec83966a865ccfd63d6
princepriyank <mishrapriyank17@gmail.com>
2021-02-22 19:38:05 +0530

    new editys

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./themes/theme";
+import { light, dark } from "./themes/theme";
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={light}>
     <App />
   </ThemeProvider>,
   document.getElementById("root")
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,4 +1,8 @@
-export const theme = {
+export const light = {
   primary: "#0b0c10",
   textColor: "#66fcf1"
 };
+export const dark = {
+  primary: "#EFF5FF",
+  textcolor: "#EFF5FF"
+}
